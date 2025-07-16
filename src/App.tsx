caeee1742c493b833b55105e425141db3c1bff8f
@keitaro-ishiya <keitaro-ishiya@internet.gmo>
2025-07-16 16:06:24 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Shion from './pages/shion'//Shionのページをインポートしてい�
 import NotFound from './pages/not-found'
 import Api from './pages/api'
 import User from './pages/user'
+import Yamaharuna from './pages/yamaharuna'
 import MahoKawase from './pages/mahokawase'
 import Ishiya from './pages/ishiya'
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/shion" element={<Shion />} />{/* Shionのページへのルートを追加 */}
         <Route path="/api" element={<Api />} />
         <Route path="/user" element={<User />} />
+        <Route path="/yamaharuna" element={<Yamaharuna />} />
         <Route path="/mahokawase" element={<MahoKawase />} />
         <Route path="/ishiya" element={<Ishiya />} />
 
